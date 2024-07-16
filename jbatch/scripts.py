cd41877b318102c36f451aa00595af53cf218821
Dmitry Solovey <dsolovey@ist.ac.at>
2024-07-16 23:33:38 +0200

    scripts.py upd

--- jbatch/scripts.py
+++ jbatch/scripts.py
@@ -1,18 +1,19 @@
-import subprocess
+import os
 import sys
+from importlib.resources import files
 
-args = sys.argv[1:]
-
-def run_bash_script(script, args):
-  try:
-    result = subprocess.run(['bash', script] + args, check=True, text=True, capture_output=True)
-    print(result.stdout)
-  except subprocess.CalledProcessError as e:
-    print(f"An error occurred while running the script: {e}")
-    print(e.stdout, e.stderr)
+def run_bash_script(script):
+  resource = files("jbatch")
+  os.environ["PATH"] = f"{resource}:{os.getenv('PATH')}"
+  script = files("jbatch").joinpath(script)
+  print(script)
+  os.execv(script, sys.argv)
 
 def err():
-  run_bash_script("./err", args)
+  run_bash_script("err")
 
 def out():
-  run_bash_script("./out", args)
+  run_bash_script("out")
+
+if __name__ == "__main__":
+  err()
